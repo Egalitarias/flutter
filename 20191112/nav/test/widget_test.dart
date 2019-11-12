f4812374cf05ca447283661f3b0db83ab5939da7
Gary Davies <garydavieshome@gmail.com>
2019-11-12 15:25:49 +0800

    nav test

--- 20191112/nav/test/widget_test.dart
+++ 20191112/nav/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:nav/main.dart';
+import 'package:nav/MyApp.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
