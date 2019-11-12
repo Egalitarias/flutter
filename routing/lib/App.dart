713dabec0205c6908c970597aa1ce03daa9f33d3
Gary Davies <garydavieshome@gmail.com>
2019-11-12 14:44:53 +0800

    routing refactoring

---
+++ routing/lib/App.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'HomePage.dart';
+
+class App extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
